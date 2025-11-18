69281d6949808d0e2085abf2bc4e8bbb3e537aba
ReivenB <164182270+ReivenB@users.noreply.github.com>
2025-11-18 22:41:03 +0800

    ActivityLog.php

---
+++ app/Models/ActivityLog.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ActivityLog extends Model
+{
+    protected $fillable = ['user_id', 'description', 'meta'];
+    protected $casts = ['meta' => 'array'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
